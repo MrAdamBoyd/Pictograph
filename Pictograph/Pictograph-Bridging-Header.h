3c4f823c7d50a354056af14231d6168146b2cfd1
Adam Boyd <adamboyd50@gmail.com>
2015-10-25 17:17:26 -0700

    replacing the Objective C AppDelegate with a Swift AppDelegate

---
+++ Pictograph/Pictograph-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#ifndef Pictograph_Bridging_Header_h
+#define Pictograph_Bridging_Header_h
+
+#import "EAIntroView/EAIntroPage.h"
+#import "EAIntroView/EAIntroView.h"
+#import "MBProgressHUD/MBProgressHUD.h"
+#import <Fabric/Fabric.h>
+#import <Crashlytics/Crashlytics.h>
+
+#endif /* Pictograph_Bridging_Header_h */
