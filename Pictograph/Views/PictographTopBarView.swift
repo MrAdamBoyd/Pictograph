f035f94113e7c4dced8339cc77453287941dc976
Adam Boyd <adamboyd50@gmail.com>
2015-10-27 10:58:49 -0700

    adding a border to the top bar

--- Pictograph/Views/PictographTopBarView.swift
+++ Pictograph/Views/PictographTopBarView.swift
@@ -17,6 +17,7 @@ private let titleLeftRightOffset: CGFloat = 65
 class PictographTopBarView: UIView {
     var titleLabel = UILabel()
     var accessoryButton = UIButton()
+    var bottomBorder = UIView()
     var accessoryButtonAction: (() -> Void)?
     
     //MARK: - UIView
@@ -27,25 +28,37 @@ class PictographTopBarView: UIView {
         //Adding the app title to the top of the screen
         titleLabel.font = UIFont.boldSystemFontOfSize(titleFontSize)
         titleLabel.textColor = UIColor.whiteColor()
-        self.addSubview(titleLabel)
+        addSubview(titleLabel)
         titleLabel.translatesAutoresizingMaskIntoConstraints = false
         titleLabel.textAlignment = .Center
         
         //Centered on y, left & right by 65px
-        self.addConstraint(NSLayoutConstraint(item: titleLabel, attribute: .CenterY, relatedBy: .Equal, toItem: self, attribute: .CenterY, multiplier: 1, constant: titleYOffset))
-        self.addConstraint(NSLayoutConstraint(item: titleLabel, attribute: .Left, relatedBy: .Equal, toItem: self, attribute: .Left, multiplier: 1, constant: titleLeftRightOffset))
-        self.addConstraint(NSLayoutConstraint(item: titleLabel, attribute: .Right, relatedBy: .Equal, toItem: self, attribute: .Right, multiplier: 1, constant: -titleLeftRightOffset))
+        addConstraint(NSLayoutConstraint(item: titleLabel, attribute: .CenterY, relatedBy: .Equal, toItem: self, attribute: .CenterY, multiplier: 1, constant: titleYOffset))
+        addConstraint(NSLayoutConstraint(item: titleLabel, attribute: .Left, relatedBy: .Equal, toItem: self, attribute: .Left, multiplier: 1, constant: titleLeftRightOffset))
+        addConstraint(NSLayoutConstraint(item: titleLabel, attribute: .Right, relatedBy: .Equal, toItem: self, attribute: .Right, multiplier: 1, constant: -titleLeftRightOffset))
         
         
         //Settings button
         accessoryButton.addTarget(self, action: Selector("buttonAction"), forControlEvents: .TouchUpInside)
         accessoryButton.setTitleColor(UIColor.whiteColor().colorWithAlphaComponent(0.5), forState: .Highlighted)
         accessoryButton.translatesAutoresizingMaskIntoConstraints = false
-        self.addSubview(accessoryButton)
+        addSubview(accessoryButton)
         
         //Bottom = titleLabel.bottom + 3, 16px from right
-        self.addConstraint(NSLayoutConstraint(item: accessoryButton, attribute: .Bottom, relatedBy: .Equal, toItem: titleLabel, attribute: .Bottom, multiplier: 1, constant: 3))
-        self.addConstraint(NSLayoutConstraint(item: accessoryButton, attribute: .Right, relatedBy: .Equal, toItem: self, attribute: .Right, multiplier: 1, constant: -16))
+        addConstraint(NSLayoutConstraint(item: accessoryButton, attribute: .Bottom, relatedBy: .Equal, toItem: titleLabel, attribute: .Bottom, multiplier: 1, constant: 3))
+        addConstraint(NSLayoutConstraint(item: accessoryButton, attribute: .Right, relatedBy: .Equal, toItem: self, attribute: .Right, multiplier: 1, constant: -16))
+        
+        
+        //Border at bottom of the bar
+        bottomBorder.backgroundColor = UIColor.whiteColor()
+        bottomBorder.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(bottomBorder)
+        
+        //Bottom = self's bottom, 0px from left, right, 1px height
+        addConstraint(NSLayoutConstraint(item: bottomBorder, attribute: .Bottom, relatedBy: .Equal, toItem: self, attribute: .Bottom, multiplier: 1, constant: 0))
+        addConstraint(NSLayoutConstraint(item: bottomBorder, attribute: .Left, relatedBy: .Equal, toItem: self, attribute: .Left, multiplier: 1, constant: 0))
+        addConstraint(NSLayoutConstraint(item: bottomBorder, attribute: .Right, relatedBy: .Equal, toItem: self, attribute: .Right, multiplier: 1, constant: 0))
+        addConstraint(NSLayoutConstraint(item: bottomBorder, attribute: .Height, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1, constant: 1))
         
     }
 
