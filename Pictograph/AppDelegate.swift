3857d328474f86404143b8fc9bf02aee353ff38d
Adam Boyd <adamboyd50@gmail.com>
2016-01-24 08:03:47 -0800

    blurring the screen when the app switcher is active

--- Pictograph/AppDelegate.swift
+++ Pictograph/AppDelegate.swift
@@ -27,6 +27,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+        
+        //Blurring view for app switcher
+        self.blurPresentedView()
     }
     
     func applicationDidEnterBackground(application: UIApplication) {
@@ -40,6 +43,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func applicationDidBecomeActive(application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        
+        //Unbluring view for app switcher
+        self.unblurPresentedView()
     }
     
     func applicationWillTerminate(application: UIApplication) {
---
+++ Pictograph/Extensions/AppDelegateExtension.swift
@@ -0,0 +1,54 @@
+//
+//  AppDelegateExtention.swift
+//  BlurInactiveScreen
+//
+//  Created by Nikolay Shubenkov on 21/05/15.
+//  Copyright (c) 2015 Nikolay Shubenkov. All rights reserved.
+//
+
+import UIKit
+
+//some random number
+private let blurViewtag = 198489
+
+extension AppDelegate {
+    
+    func blurPresentedView() {
+        //return if bluered view with hardcoded tag is added to main window
+        if (self.window?.viewWithTag(blurViewtag) != nil){
+            return
+        }
+        
+        let snapshot  = bluredSnapshot();
+        self.window?.addSubview(snapshot!)
+    }
+    
+    //find and remove blured view
+    func unblurPresentedView() {
+        self.window?.viewWithTag(blurViewtag)?.removeFromSuperview()
+    }
+    
+    func bluredSnapshot () -> UIView? {
+        //take window snapshot
+        //and add blurView to it
+        let snapshot = self.window?.snapshotViewAfterScreenUpdates(true)
+        snapshot?.addSubview(blurView((snapshot?.frame)!))
+        snapshot?.tag = blurViewtag
+        return snapshot
+    }
+    
+    func blurView(frame: CGRect)->UIView {
+        //iOS 8 and later
+        switch UIDevice.currentDevice().systemVersion.compare("8.0.0", options: NSStringCompareOptions.NumericSearch) {
+        case .OrderedSame, .OrderedDescending:
+            let view    = UIVisualEffectView(effect: UIBlurEffect(style: .Dark))
+            view.frame  = frame
+            return view
+            //Other
+        case .OrderedAscending:
+            let toolbar      = UIToolbar(frame: frame)
+            toolbar.barStyle = UIBarStyle.BlackTranslucent;
+            return toolbar
+        }
+    }
+}
