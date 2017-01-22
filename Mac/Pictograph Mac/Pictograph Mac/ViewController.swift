608d64e9640614d338686d08633cd01e783fa4bd
Adam Boyd <adamboyd50@gmail.com>
2017-01-22 12:19:06 -0800

    reorganized everything for mac app

---
+++ Mac/Pictograph Mac/Pictograph Mac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Pictograph Mac
+//
+//  Created by Adam Boyd on 2017-01-22.
+//  Copyright © 2017 adam. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
