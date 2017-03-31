ce5a13dfd4e7f10465ed5f0378275c0027ab0491
Adam Boyd <adamboyd50@gmail.com>
2017-03-30 19:54:32 -0700

    putting all the pieces together, ui mostly set up, about to call encoding method

--- Project/Mac/Pictograph Mac/MainViewController.swift
+++ Project/Mac/Pictograph Mac/MainViewController.swift
@@ -8,7 +8,7 @@
 
 import Cocoa
 
-class MainViewController: NSViewController {
+class MainViewController: NSViewController, NSTextFieldDelegate {
 
     @IBOutlet weak var mainImageView: NSImageView!
     @IBOutlet weak var encryptionCheckbox: NSButton!
@@ -16,6 +16,10 @@ class MainViewController: NSViewController {
     
     @IBOutlet weak var hideMessageButton: NSButton!
     @IBOutlet weak var showMessageButton: NSButton!
+    @IBOutlet weak var messageTextField: NSTextField!
+    
+    
+    var imageSelectPanelOpen: Bool = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,8 +27,20 @@ class MainViewController: NSViewController {
         // Do any additional setup after loading the view.
         self.mainImageView.wantsLayer = true
         self.mainImageView.layer?.borderColor = NSColor.black.cgColor
-        self.mainImageView.layer?.borderWidth = 10
+        self.mainImageView.layer?.borderWidth = 2
         self.mainImageView.layer?.masksToBounds = true
+        
+        let clickGR = NSClickGestureRecognizer(target: self, action: #selector(self.selectNewImageFromFileSystem))
+        self.mainImageView.addGestureRecognizer(clickGR)
+        
+        self.messageTextField.delegate = self
+        
+        self.checkIfValid()
+        
+        //LOOK AT THIS::::::!!!!!!!!!!!!!!!!
+        //https://www.raywenderlich.com/136272/drag-and-drop-tutorial-for-macos
+        //FOR DRAGGING
+        //ALSO ONLY INCLUDE IMAGES IN NSOPENPANEL
     }
 
     override var representedObject: Any? {
@@ -33,14 +49,78 @@ class MainViewController: NSViewController {
         }
     }
     
-    @IBAction func encryptionEnabledChanged(_ sender: NSButton) {
-        self.passwordTextfield.isEnabled = sender.state == 1
+    /// Enables or disables the hide and show message buttons based on the state
+    func checkIfValid() {
+        //Valid if encryption disabled OR encryption enabled and textfield isn't empty
+        let encryptionValid = self.encryptionCheckbox.state == 0 || (self.encryptionCheckbox.state == 1 && !self.passwordTextfield.stringValue.isEmpty)
+    
+        let imageValid = self.mainImageView.image != nil
+        
+        let hideMessageValid = !self.messageTextField.stringValue.isEmpty
+        
+        self.showMessageButton.isEnabled = encryptionValid && imageValid
+        self.hideMessageButton.isEnabled = encryptionValid && imageValid && hideMessageValid
+    }
+    
+    // MARK: - User actions
+    
+    func selectNewImageFromFileSystem() {
+        
+        guard !self.imageSelectPanelOpen else { return }
+        
+        self.imageSelectPanelOpen = true
+        
+        print("Getting image")
+        let panel = NSOpenPanel()
+        panel.begin() { result in
+            self.imageSelectPanelOpen = false
+            if let fileUrl = panel.url, result == NSFileHandlingPanelOKButton {
+                guard let image = NSImage(contentsOf: fileUrl) else { return }
+                
+                self.mainImageView.image = image
+            }
+            
+            self.checkIfValid()
+        }
+    }
+    
+    @IBAction func encryptionEnabledChanged(_ sender: Any) {
+        self.passwordTextfield.isEnabled = self.encryptionCheckbox.state == 1
     }
     
     @IBAction func hideMessageAction(_ sender: Any) {
         print("User wants to hide message")
     }
 
+    @IBAction func showMessageAction(_ sender: Any) {
+        print("User wants to show message")
+        
+        //Dispatching the task after  small amount of time as per SVProgressHUD's recommendation
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.01) {
+            let coder = UIImageCoder()
+            
+            do {
+//                let encodedImage = try coder.encodeMessage(messageToEncode, in: self.mainImageView.image!, encryptedWithPassword: PictographDataController.shared.userEncryptionPassword)
+                let encodedImage = try coder.encodeMessage(self.messageTextField.stringValue, in: self.mainImageView.image!, encryptedWithPassword: nil)
+                self.mainImageView.image = NSImage(data: encodedImage)
+                //Show the share sheet if the image exists
+//                self.showShareSheetWithImage(encodedImage)
+                
+            } catch let error {
+                
+                //Catch the error
+//                self.showMessageInAlertController("Error", message: error.localizedDescription)
+            }
+        }
+    }
+    
+    // MARK: - NSTextFieldDelegate
+    
+    /// Called every time the user typed a key in the text field
+    override func controlTextDidChange(_ obj: Notification) {
+        print("User entered text")
+        self.checkIfValid()
+    }
 
 }
 
---
+++ Project/Mac/Pictograph Mac/Pictograph_Mac-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Pictograph_Mac-Bridging-Header.h
+//  Pictograph
+//
+//  Created by Adam Boyd on 2017-03-30.
+//  Copyright © 2017 Adam Boyd. All rights reserved.
+//
+
+#ifndef Pictograph_Mac_Bridging_Header_h
+#define Pictograph_Mac_Bridging_Header_h
+
+#import "UIImageCoder.h"
+
+#endif /* Pictograph_Mac_Bridging_Header_h */
