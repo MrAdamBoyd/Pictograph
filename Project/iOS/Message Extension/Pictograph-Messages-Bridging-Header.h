93c4a1c099f29e2b99c179aea40e003845eb9b93
Adam Boyd <adamboyd50@gmail.com>
2017-05-29 14:28:56 -0700

    reorganizing the project

---
+++ Project/iOS/Message Extension/Pictograph-Messages-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  Pictograph-Messages-Bridging-Header.h
+//  Pictograph
+//
+//  Created by Adam Boyd on 2017/5/9.
+//  Copyright © 2017 Adam Boyd. All rights reserved.
+//
+
+#ifndef Pictograph_Messages_Bridging_Header_h
+#define Pictograph_Messages_Bridging_Header_h
+
+#import "PictographImageCoder.h"
+#import "PictographImage+Reconciliation.h"
+
+#endif /* Pictograph_Messages_Bridging_Header_h */
